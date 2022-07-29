d09404744189a4afb8379fd7553035af53f9f79b
Manish Kesarwani <manish@Manishs-MacBook-Pro.local>
2022-07-29 20:06:06 +0530

    Updated Executables and Helper functions

---
+++ executables/ExecuteJar.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package helper;
+
+import java.io.IOException;
+
+/**
+ * @author manish
+ *
+ */
+public class ExecuteJar 
+{
+	
+	public static void executeJar(String jarFilePath, String args) 
+	{
+		int exitVal;
+	    
+	    String str_cmd = "java -jar " + jarFilePath + " " + args;
+	    try {
+	        final Runtime re = Runtime.getRuntime();
+	        final Process command = re.exec(str_cmd);
+	        // Wait for the application to Finish
+	        command.waitFor();
+	        exitVal = command.exitValue();
+	        if (exitVal != 0) {
+	            throw new IOException("Failed to execure jar " );
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		String runArgs = "-o \"5432\" -d \"postgres\" -u \"postgres\" -p \"\" -l \"localhost\" -t \"studrq22\"";
+		 
+		 String jarFile = "/Users/manish/executable/Q3.jar";
+		    ExecuteJar.executeJar(jarFile, runArgs);;
+		    
+	}
+
+}
